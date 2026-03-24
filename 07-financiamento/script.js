17e1bae7e4985beee1c0c557a7332914520ad03c
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-03-24 17:05:34 -0300

    wip: projeto em andamento

---
+++ 07-financiamento/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const qtdeMeses = function(quantidade){
+    let meses = []
+
+    for(let i = 1; i <= quantidade; i++)
+        meses.push(i)
+}
+
+const calcularParcela = (valorTotal, qtdeParcela) => Number(valorTotal / qtdeParcela)
